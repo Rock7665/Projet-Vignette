157ce8e57b26035c0617821542cd0c4548a27199
Rock7665 <mouton7665@gmail.com>
2023-10-25 11:36:26 +0000

    Dynamiser le resultat lorsque les champs sont remplis au lieu du clic sur le bouton de soumission

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 $(function() {
-    $('#form').submit(function() {
-      var selectItem = $('#select-form').val();
-      var textItem = $('#text-form').val();
-      if (textItem == '') {
-        $('#error-message').text(`Entrer s'il vous plait la puissance!`);
-      } else {
-        $('#error-message').text('');
-      }
-      $('#output-select').text(selectItem);
-      $('#output-text').text(textItem);
-      return false;
-    });
-  
-    $('.option-btn').click(function() {
-      var optionText = $(this).text();
-      // Lorsque c'est cliquer dessus, stockez la valeur de l'élément data-option dans la variable clickedOption
-      var clickedOption = $(this).attr('data-option');
-      
-      $('#text-form').val(optionText + ' Puissance Fiscale:');
-      // clickedOption pour saisir automatiquement la valeur du #select-form
-      $('#select-form').val(clickedOption);
-      
-    });
+  // Écoutez l'événement "input" sur les champs de texte
+  $('#select-form, #text-form').on('input', function() {
+    var selectItem = $('#select-form').val();
+    var textItem = $('#text-form').val();
+    $('#output-select').text(selectItem);
+    $('#output-text').text(textItem);
   });
+
+  // Ajoutez un gestionnaire d'evenement sur chaque bouton
+  $('.option-btn').click(function() {
+    var optionText = $(this).text();
+    // Lorsque c'est cliquer dessus, stockez la valeur de l'élément data-option dans la variable clickedOption
+    var clickedOption = $(this).attr('data-option');
+    
+    $('#text-form').val(optionText + ' Puissance Fiscale:');
+    // clickedOption pour saisir automatiquement la valeur du #select-form
+    $('#select-form').val(clickedOption);
+    
+  });
+});
